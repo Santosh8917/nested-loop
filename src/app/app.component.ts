ba6b72ae32e6cd39eeda57c0284ee4c8dd1f2192
Santosh Behera01 <Santosh.Behera01@godigit.com>
2023-01-22 23:01:12 +0530

    Nested Loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'loop';
+  title = 'Nested For Loop';
+
+users=[
+  {name:"anil",email:"anil@gmail.com",phone:7778889,accounts:['facebook','email','linkedin']},
+  {name:"ram",email:"ram@gmail.com",phone:6677778,accounts:['Youtube','yahoo','linkedin']},
+  {name:"peter",email:"peter@gmail.com",phone:999999,accounts:['yahoo','insta','linkedin']},
+  {name:"bruce",email:"bruce@gmail.com",phone:10000,accounts:['whatsapp','gmail','linkedin']}
+]
+
+
 }
